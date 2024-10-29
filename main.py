4b29d787d83f7ef501d2c8cdcab74aa5c7b7c26a
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-10-29 11:21:01 -0500

    Tipos de datos en python

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-name = input("Enter your name: c")
-message = f"Hello wordl {name}"
-print(message)
+fullName = "Miguel Angel Castro Escamilla"  #Tipo de datos string
+gender = "M" #Tipo de dato char
+age = 22 #Tipo de dato int
+height = 1.687 #Tipo de dobule 
+trainer = True #Tipo de dato boolean
+
+# Conjuntos
+hobbies = ["Pragarmar", 'Coninar', """Wow Horda"""] #Tipo de dato array
+direction = (dict) ["country": "Colombia", "state": "Santander"] #Tipo de dato object
+
+print(f""" Teacher's Name: "{fullName}" """)

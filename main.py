962eb550897ee4f62f60cf64ed5e69bbc946915d
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-10-30 12:26:25 -0500

    Operadores de comparacion y condicionales ramificados secillos

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-fullName = input("Enter your full name: ")
-age = int(input("Enter your age: "))
-gender = input("Enter your possible gender options M, F, X: ")
-heigth = float(input("Enter your height: "))
+#  > Mayor
+#  < Menor
+#  >= Mayor igual
+#  <= Menor igual
+#  == igual
+#  != Diferente
+#  !==  No identico
+# 
+# Estos operador de comparacion 
+# devuelven una afirmacion boleana si se cumple o no 'True' 'False'
 
-print(f"""
-Hola, usuario {fullName}. ¿Cómo estás? Te saluda Python. 
-Gracias por compartir tu edad {age}; no sabía que eres un humano de tipo {gender}.
-""")
+age = int(input("Ingrese su edad: ")) # 20
+#   20 == 18
+#     False
+if age == 18:
+    print("El mayor de edad")
+#  20 == 20
+#    True
+elif age == 20:
+    print("El mayor de edad y nosabia que tienes 20")
+else:
+    print("Nose :( lo que eres")
